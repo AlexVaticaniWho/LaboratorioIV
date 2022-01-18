070da5109197117b47c6644e8ddbbf89967c3c59
AlexVaticaniWho <90463645+AlexVaticaniWho@users.noreply.github.com>
2022-01-18 23:31:42 +0100

    Corretta indentazione, aggiunti millisecondi print

--- read_rx.c
+++ read_rx.c
@@ -26,11 +26,10 @@ void time_diff(struct timeval t_start, struct timeval t_end, struct timeval *td)
 int main(int argc, char *argv[]){
 
   //inizializzo
-  struct tm *gmp;
+  struct tm *gmp, *gmp_run;
   struct timeval start, end, diff_time; 
   time_t t, t0;
   int ty, tmon, tday, thour, tmin, tsec, time_acq_h_MAX;
-  float time_acq_h;
   int i, n, nloc, InitFlag, StartFlag, nhit, hit, trg, cport_nr=17, bdrate=115200;  
 
   int cnt=0;
@@ -93,116 +92,101 @@ int main(int argc, char *argv[]){
   InitFlag=0;
   nloc=0;
   trg=0;
-  
+  gettimeofday(&start, NULL);
   while(1){     //itero per un numero infinito di volte (condizione sempre verificata)
-  
-  
   
     n = RS232_PollComport(cport_nr, buf, 4095); //numero di bytes ricevuti attraverso la porta seriale, messi in buf il cui massimo valore rappresentabile � 4095
     
-    
-      /*********stimo la durata dell'acquisizione dati**************/
-      
-      t = time(NULL);
-      gmp = gmtime(&t);
-      if (gmp == NULL)
-        printf("error on gmp");
-      printf("%d %d ", cnt, n);
+    /*********stimo la durata dell'acquisizione dati**************/      
+    t = time(NULL);
+    gmp_run = gmtime(&t);
+    if (gmp == NULL) printf("error on gmp");
 
-      gm = *gmp;   
-     
-      time_acq_h=(t-t0);
-     
-     printf("time diff: %f (sec) \n", time_acq_h);
-     
-     if (time_acq_h> time_acq_h_MAX*3600){
-     	
-          printf(" time_duration RUN in minutes > %d \n",time_acq_h_MAX*60); 
-          break;
-        }  
-     else{
-     	
-	  if (cnt%100==0)                                                     //cambia alla fine del while(1)
-            printf(" time current in hour %f \n",(float)time_acq_h/3600.);
-        }
+    gettimeofday(&end, NULL);
+    time_diff(start, end, &diff_time);
+
+    printf("%d %d ", cnt, n);
+    
+    printf("time diff: %ld (sec) \n", diff_time.tv_sec);
+    
+    if ( diff_time.tv_sec >  time_acq_h_MAX*3600){	
+      printf(" time_duration RUN in minutes > %d \n",time_acq_h_MAX*60); 
+      break;
+    }  
+    
+    else if (cnt%100==0) printf(" time current in hour %ld \n", diff_time.tv_sec/3600);        //cambia alla fine del while(1)
         
         
     /***********acquisizione dati*************/
     
-    if(n > 0){
-    	
-    	
-       buf[n] = 0; // l'(n+1)esimo byte � nullo quando stiamo acquisendo i primi n
-       nhit=n/6;  // numero di pacchetti di dati (due bytes di controllo, due di RH e due di T)
-       hit=0; 
-       printf("nhit %d \n",nhit); 
-    
-    
-       for(i=0; i < n; i++){
+    if(n > 0) {	
+      buf[n] = 0; // l'(n+1)esimo byte � nullo quando stiamo acquisendo i primi n
+      nhit=n/6;  // numero di pacchetti di dati (due bytes di controllo, due di RH e due di T)
+      hit=0; 
+      printf("nhit %d \n",nhit); 
+  
+  
+      for(i=0; i < n; i++){
 
-	    if (InitFlag==1){ //parte dal terzo byte, dopo il primo di controllo
+	      if (InitFlag==1){ //parte dal terzo byte, dopo il primo di controllo
        
-      	    sht75_nblab03_frame[nloc]=buf[i]; //a partire dal terzo byte riempie il primo bit corrispondente a nloc=0
+          sht75_nblab03_frame[nloc]=buf[i]; //a partire dal terzo byte riempie il primo bit corrispondente a nloc=0
 
-	        if (nloc==1){
+          if (nloc==1){
 	        	
-              val_hum_int=(sht75_nblab03_frame[0]<<8)|(sht75_nblab03_frame[1]); /*OR (restituisce un int) tra il primo byte traslato e il secondo;
-                                                                                in questo modo mettiamo in sequenza i due byte di umidit� relativo e li leggiamo
-                                                                                come un unico valore binario*/
-                                                                                
-	          val_hum=decodeHumidity(val_hum_int,val_temp);
-	          
-
-              trg++;
-              hit++; 
-            }
+            val_hum_int=(sht75_nblab03_frame[0]<<8)|(sht75_nblab03_frame[1]); /*OR (restituisce un int) tra il primo byte traslato e il secondo;
+                                                                                  in questo modo mettiamo in sequenza i due byte di umidit� relativo e li leggiamo
+                                                                                  come un unico valore binario*/                                                                       
+            val_hum=decodeHumidity(val_hum_int,val_temp);
+            trg++;
+            hit++; 
+          }
             
-      	else if(nloc==3){
+      	  else if(nloc==3){
       		
-              val_temp_int=(sht75_nblab03_frame[2]<<8)|(sht75_nblab03_frame[3]);
+            val_temp_int=(sht75_nblab03_frame[2]<<8)|(sht75_nblab03_frame[3]);
 	          val_temp=decodeTemperature(val_temp_int);
 	          val_hum_corr=corrHumidity(val_hum, val_hum_int, val_temp);
 	          
-              if (cnt%100==0){
-            printf(" read_Hum MSB %x - LSB %x --> Hum16bitRaw  %x - HumReco %.2f (dec)\n",sht75_nblab03_frame[0],sht75_nblab03_frame[1],val_hum_int, val_hum_corr);
-	      	printf(" read Temp MSB %x - LSB %x --> Temp16bitRaw %x - TempReco %.2f (dec)\n",sht75_nblab03_frame[2],sht75_nblab03_frame[3],val_temp_int,val_temp);
-		    }
+            if (cnt%100==0){
+              printf(" read_Hum MSB %x - LSB %x --> Hum16bitRaw  %x - HumReco %.2f (dec)\n",sht75_nblab03_frame[0],sht75_nblab03_frame[1],val_hum_int, val_hum_corr);
+	          	printf(" read Temp MSB %x - LSB %x --> Temp16bitRaw %x - TempReco %.2f (dec)\n",sht75_nblab03_frame[2],sht75_nblab03_frame[3],val_temp_int,val_temp);
+		        }
 		    
-	      	fprintf(file,"%d\t%d\t%d\t%d\t%.1f\t", trg, gm.tm_year+1900,gm.tm_mon+1, gm.tm_mday, 3600*gm.tm_hour + 60*gm.tm_min+gm.tm_sec+(double)hit*4/(double)nhit); 
+            fprintf(file,"%d\t%d\t%d\t%d\t%ld(ms)\t", trg, gmp_run->tm_year+1900,gmp_run->tm_mon+1, gmp_run->tm_mday, 3600000*gmp_run->tm_hour + 60000*gmp_run->tm_min + 100*gmp_run->tm_sec + end.tv_usec/1000); 
             fprintf(file,"\t%d\t%.2f\t",val_hum_int,val_hum_corr);
             fprintf(file,"%d\t%.2f\n",val_temp_int,val_temp);
-	    }
+	        }
 	    
-	    else if (nloc>5){
-	      printf(" error on nloc\n");
-	    }
-	    
+          else if (nloc>5){
+            printf(" error on nloc\n");
+          } 
         }
          
         nloc++; //faccio crescere ad ogni iterazione nloc che tiene conto delle coppie di bytes acquisite
 
-         if (i>0 && buf[i]==0xAA && buf[i-1]==0xAA) //&& se entrambi gli operatori sono diversi da zero (veri) � una condizione vera
-                                                    //0x sta per "la seguente � una cifra esadecimale" AA � il byte di controllo in esadecimale 
-													//cerchiamo due bytes di controllo AA consecutivi
-           {
-             StartFlag=1;
-             nloc=0;
-             
-	        if (InitFlag==0)
-                {
-                 InitFlag=1;
-               }
-           }
-      }
+        if (i>0 && buf[i]==0xAA && buf[i-1]==0xAA) //&& se entrambi gli operatori sono diversi da zero (veri) � una condizione vera
+                                                  //0x sta per "la seguente � una cifra esadecimale" AA � il byte di controllo in esadecimale 
+                                                  //cerchiamo due bytes di controllo AA consecutivi
+        {
+          StartFlag=1;
+          nloc=0;
+          
+          if (InitFlag==0)
+          {
+            InitFlag=1;
+          }
+        }
+      } 
       printf("cnt %d received %i bytes \n", cnt, n);
-     cnt++;
+      cnt++;
     }
 
-#ifdef _WIN32
-    Sleep(400); //sospende temporaneamente il processo per 400ms
-#else
-    usleep(400000); 
-#endif
+    #ifdef _WIN32
+        Sleep(400); //sospende temporaneamente il processo per 400ms
+    #else
+        usleep(400000); 
+    #endif
   }
  
   fclose (file);
@@ -217,7 +201,7 @@ double decodeTemperature(unsigned int rbuf) {
   d1=-39.6;
   d2=0.01;
   rd_val=(double)rbuf + 0.;
- return d1+d2*rd_val ;
+  return d1+d2*rd_val ;
 }
 
 double decodeHumidity(unsigned int rbuf, double temperature_ref){
@@ -227,7 +211,7 @@ double decodeHumidity(unsigned int rbuf, double temperature_ref){
   c3=-1.5955e-6;
   rd_val=(double)rbuf;
   hum_val=c1+c2*rd_val+c3*(rd_val)*(rd_val);
- return hum_val ;  
+  return hum_val ;  
 }
 
 double corrHumidity(double hum_val, unsigned int rbuf, double temperature_ref){
@@ -244,7 +228,7 @@ void time_diff(struct timeval t_start, struct timeval t_end, struct timeval *td)
   td->tv_sec =  t_end.tv_sec - t_start.tv_sec;
 
   if (td->tv_sec > 0 && td->tv_usec < 0) {
-      td->tv_usec += NUM;
+      td->tv_usec += 1000000;
       td->tv_sec--;
   }
 }
